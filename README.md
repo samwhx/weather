26ebc61a7f7adffa537475cb22ec3896a8317c3e
Samuel Wang <samuelwhx@gmail.com>
2018-09-07 00:37:31 +0800

    minor bug fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Weather3
+# Weather
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.5.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'weather3'`, async(() => {
+  it(`should have as title 'weather'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('weather3');
+    expect(app.title).toEqual('weather');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to weather3!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to weather!');
   }));
 });
